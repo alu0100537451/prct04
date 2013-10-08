98fb0318c67ee8c3311a1d6d335c675cb13f22d3
Aaron Vera <alu4138@cc194.(none)>
2013-10-08 18:21:29 +0100

    Modificaciones rama matriz

--- prct04.rb
+++ prct04.rb
@@ -3,15 +3,19 @@
 puts "Suma y multiplicacion de dos matrices cuadradas\n"
 
 # Matriz x
+
 x = [[6, 1, 3], [4, 3, 1], [7, 2, 1]]
 
+
 # Matriz y
+
 y = [[2, 2, 5], [4, 3, 6], [2, 5, 8]]
 
 mul = Array.new(3){Array.new(3)}
 sum = Array.new(3){Array.new(3)}
 
 # Suma
+
 for i in (0...3)
         for j in (0..2)
                 sum[i][j] = x[i][j] + y[i][j]
@@ -19,6 +23,8 @@ for i in (0...3)
 end
 
 # Suma de matrices
+
+
 for i in (0..2)
         for j in (0...3)
                 temp = 0
